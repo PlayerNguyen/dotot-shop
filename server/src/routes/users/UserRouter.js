6232657810d80d653e213020d189d68464785d14
Player Nguyên <daudaua3782@gmail.com>
2022-09-28 12:56:57 +0700

    Lint UserRouter.js

--- server/src/routes/users/UserRouter.js
+++ server/src/routes/users/UserRouter.js
@@ -1,5 +1,7 @@
+"use strict";
 const express = require("express");
 const { createUser } = require("./UserController");
+// eslint-disable-next-line
 const router = express.Router();
 
 router.post(`/`, createUser);
