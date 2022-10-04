8aa375ef32ad9caf81d96866cb7a1e638e1ee65f
Player Nguyên <daudaua3782@gmail.com>
2022-10-04 21:40:44 +0700

    add login routes for auth router

--- server/src/routes/auth/AuthRouter.js
+++ server/src/routes/auth/AuthRouter.js
@@ -1,7 +1,31 @@
 "use strict";
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 
-router.post("/login");
+const passport = require("passport");
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const { verifyUserAndResponseToken } = require("./AuthController");
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET_OR_KEY,
+    },
+    function verify(payload, done) {
+      console.log(payload);
+    },
+  ),
+);
+
+router.post(
+  "/login",
+  check("phoneOrEmail", "phoneOrEmail cannot be empty"),
+  check("password", "password cannot be empty")
+    .isLength({ min: 5 })
+    .withMessage("Password must have 5+ characters"),
+  verifyUserAndResponseToken,
+);
 
 module.exports = router;
