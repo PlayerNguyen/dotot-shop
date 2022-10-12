1e4c2f589bab72cc28efba90492507db2df3b49e
Player Nguyên <daudaua3782@gmail.com>
2022-10-12 11:36:31 +0700

    Add UserRole test package

---
+++ server/test/UserRole.spec.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * TODO: implement this test package
+ * - Register, sign up and check for the profile response must have role with admin
+ *
+ */
+
+describe(`UserRole`, () => {
+  it(`successfully response`, () => {
+    throw new Error("Not implementation");
+  });
+});
