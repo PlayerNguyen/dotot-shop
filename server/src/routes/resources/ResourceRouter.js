c5514fe5bcf27859cdf251277bb34cdd2cbc3481
Player Nguyên <daudaua3782@gmail.com>
2022-11-10 10:38:22 +0700

    Add resource router

---
+++ server/src/routes/resources/ResourceRouter.js
@@ -0,0 +1,10 @@
+"use strict";
+const express = require("express");
+const ResourceRouter = express.Router();
+
+/**
+ * Upload new image
+ */
+ResourceRouter.post("/", () => {});
+
+module.export = ResourceRouter;
