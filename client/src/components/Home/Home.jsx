36d28f584124f53bce9ff3540ba0363d6377628d
Player Nguyên <daudaua3782@gmail.com>
2022-11-15 23:57:53 +0700

    Add home, show products for mobile

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductItemCard from "../ProductItemCard/ProductItemCard";
 
 export default function Home() {
+  const [products, setProducts] = useState([
+    {
+      name: "Modern Chair Desk",
+      price: "38.25",
+      id: "123",
+      condition: "99%",
+      image: `https://dummyimage.com/600x400/000/fff`,
+    },
+    {
+      name: "Giant Vertux Lamp",
+      price: "129.25",
+      id: "39x9",
+      condition: "99%",
+      image: `https://dummyimage.com/600x400/000/fff`,
+    },
+  ]);
   return (
-    <div className="home-wrapper mx-14 my-12 bg-white px-8 py-4 rounded-lg">
+    <div className="home-wrapper mx-4 px-6 pb-4 rounded-lg">
       <div className="product-wrapper">
-        <div className="font-bold text-2xl mb-3">New arrivals</div>
+        <div className="font-bold text-5xl my-8 font-serif text-center">
+          New arrivals
+        </div>
 
-        <div className="product-items">
-          {[...Array(5)].map((_, _i) => {
-            <ProductItemCard
-              name={`Old spicy kernel with more fits`}
-              price={"125.25"}
-              id={`91070173890-81930`}
-            />;
-          })}
+        <div className="product-items block">
+          {products.map((_, _i) => (
+            <div className="w-1/2 px-4 py-4 inline-block">
+              <ProductItemCard
+                name={_.name}
+                price={_.price}
+                id={_.id}
+                condition={_.condition}
+                image={_.image}
+                key={_.id}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- client/src/components/ProductItemCard/ProductItemCard.jsx
+++ client/src/components/ProductItemCard/ProductItemCard.jsx
@@ -1,24 +1,43 @@
 import React from "react";
+import { FiShoppingCart } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
-export default function ProductItemCard({ name, price, condition, id }) {
+export default function ProductItemCard({ name, price, condition, id, image }) {
   return (
-    <Link className="productCard-wrapper cursor-pointer" to={`/products/${id}`}>
+    <Link
+      className={`
+      productCard-wrapper block bg-zinc-100 rounded-xl
+      cursor-pointer border border-zinc-400 hover:shadow-lg 
+      transition-shadow ease-in-out`}
+      to={`/products/${id}`}
+    >
       {/* Header */}
       <div className="productCard-header">
         <div className="productCard-thumbnail-wrapper">
-          <img className="bg-gray-100 w-full h-[180px]" />
+          <img
+            className="bg-transparent w-full h-[180px] rounded-t-lg"
+            src={image}
+          />
         </div>
       </div>
 
-      <div className="flex flex-col gap-1 mt-1 items-start">
-        <div className="flex flex-row gap-2 items-center">
-          <span className="bg-yellow-300 text-yellow-700 px-2 py-0.5 rounded-lg">
-            medium
+      <div className="flex flex-col gap-1 mt-1 items-start px-3 py-4">
+        <div className="flex flex-row gap-2 items-center flex-1 w-full">
+          <span className="bg-green-400 px-2 py-0.5 rounded-lg text-xs">
+            {condition}
           </span>
-          <span className="text-stone-400">{name}</span>
         </div>
-        <p className="font-bold text-red-500">$ {price}</p>
+        <div>
+          <span className="text-zinc-600">{name}</span>
+        </div>
+        <div className="w-full"></div>
+
+        <div className="w-full flex flex-row-reverse items-center">
+          <span className="mr-4 text-xl hover:bg-zinc-200 rounded-full p-2">
+            <FiShoppingCart />
+          </span>
+          <p className="font-bold text-black flex-1 text-2xl">$ {price}</p>
+        </div>
       </div>
     </Link>
   );
