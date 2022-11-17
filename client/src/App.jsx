df3b419640bc3958897c7494d46b77da40578183
Player Nguyên <daudaua3782@gmail.com>
2022-11-17 10:24:29 +0700

    Add search bar and restyle navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { FiMenu } from "react-icons/fi";
-import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
+
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
@@ -8,6 +7,7 @@ import SignUp from "./components/Credentials/SignUp";
 import Credentials from "./components/Credentials/Credentials";
 import NoMatch from "./components/NoMatch/NoMatch";
 import SignIn from "./components/Credentials/SignIn";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
   return (
@@ -16,53 +16,7 @@ export default function App() {
 
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
-        <div className="navbar bg-base-100 lg:px-12">
-          {/* Navbar start section */}
-          <div className="flex flex-row lg:hidden">
-            <label htmlFor="app-drawer" className="drawer-button btn btn-ghost">
-              <FiMenu className="text-xl" />
-            </label>
-          </div>
-
-          <div className="flex-1 px-3">
-            <Link to="/">
-              <h1 className="text-xl font-bold">Khmer Furniture</h1>
-            </Link>
-          </div>
-
-          {/* Wide screen  */}
-          <div className="hidden lg:block">
-            <div className="menu menu-horizontal p-2 rounded-box">
-              <li>
-                <a>Home</a>
-              </li>
-              <li>
-                <a>About</a>
-              </li>
-            </div>
-          </div>
-
-          {/* For small screen */}
-          <div className="lg:hidden">
-            <div className="menu menu-horizontal p-2 rounded-box">
-              {/* Shopping cart */}
-              <li>
-                <a className="text-3xl relative">
-                  <AiOutlineShoppingCart />
-                  <span className="absolute text-sm left-2 top-2 bg-red-400 px-2 rounded-full text-white">
-                    3
-                  </span>
-                </a>
-              </li>
-              {/* Search */}
-              <li>
-                <a className="text-3xl relative">
-                  <AiOutlineSearch />
-                </a>
-              </li>
-            </div>
-          </div>
-        </div>
+        <Navbar />
 
         {/* Render home */}
         <Routes>
