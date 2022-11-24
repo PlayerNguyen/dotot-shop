171b448ee3be5c28849946b640cd0f04d9899af2
Player Nguyên <daudaua3782@gmail.com>
2022-11-24 12:05:22 +0700

    Insert sale product and condition generator

--- server/scripts/product-generator.js
+++ server/scripts/product-generator.js
@@ -7,11 +7,16 @@ const Tables = require("../driver/Table");
 const { v4: uuid } = require("uuid");
 const chalk = require("chalk");
 
+const flip = () => {
+  return Math.random() <= 0.1;
+};
 /**
  * Startup
  */
 (async () => {
   console.log(`Generating a thousand products`);
+  const salesIds = [];
+  // Generate the product
   for (let i = 0; i < 1000; i++) {
     const generatedProduct = {
       Id: uuid(),
@@ -24,9 +29,31 @@ const chalk = require("chalk");
       ],
     };
 
+    // Flip coin for sales or not?
+    if (flip()) {
+      salesIds.push({ Id: generatedProduct.Id, Price: generatedProduct.Price });
+    }
+
     // eslint-disable-next-line
     await KnexDriver(Tables.Products).insert(generatedProduct);
     console.log(generatedProduct);
+  }
+
+  // Generating some sales
+  console.log(`Generating sales for a set of product`);
+
+  for (let i = 0; i < salesIds.length; i++) {
+    const currentItem = salesIds[i];
+    // From 20% to 60%
+    const saleRatio = Math.abs(Math.random() * (0.2 - 0.6) + 0.2);
+    // Multiply ratio with sale price
+    const salePrice = currentItem.Price * saleRatio;
+
+    console.log(`Set sale for the product ${currentItem.Id}`);
+    const items = { ProductId: currentItem.Id, SalePrice: salePrice };
+    // eslint-disable-next-line
+    await KnexDriver(Tables.SaleProducts).insert(items);
+    console.log(items);
   }
 })()
   .then(() => {
