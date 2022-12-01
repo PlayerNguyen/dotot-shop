763eefb7bcf72828e79197ba1cdf29f101728c99
Player Nguyên <daudaua3782@gmail.com>
2022-12-01 10:21:15 +0700

    Add checkout for cart items

--- client/src/components/Checkout/Checkout.jsx
+++ client/src/components/Checkout/Checkout.jsx
@@ -2,24 +2,60 @@ import React, { useState } from "react";
 import CheckoutItem from "./CheckoutItem";
 
 export default function Checkout() {
-  const [items, setItems] = useState(["a"]);
+  const [items, setItems] = useState(["", "", ""]);
+  const [purchaseLoading, setPurchaseLoading] = useState(false)
+
   return (
     <div className="checkOut-wrapper">
       {/* Checkout container */}
-      <div className="checkOut-container mx-auto w-full sm:w-3/4 md:w-2/3  bg-base-100 px-12 py-4 flex flex-col">
+      <div className="checkOut-container mx-auto w-full sm:w-3/4 md:w-2/3  bg-base-100 px-12 py-4 flex flex-col gap-6">
         {/* Header */}
         <div className="checkOut-header flex flex-col">
           <div className="font-bold text-4xl">Checkout</div>
-          <div className="text-base-300">small item here</div>
+          {/* <div className="text-base-300">small item here</div> */}
         </div>
 
         {/* Body */}
-        <div className="checkOut-body flex flex-col">
+        <div className="checkOut-body flex flex-col gap-4">
           {/* List of items */}
           {items &&
             items.map((item, _index) => {
               return <CheckoutItem />;
             })}
+        </div>
+        <hr />
+        {/* Footer */}
+        <div className="flex flex-col gap-4">
+          {/* Listing all items */}
+          <div className="flex flex-row">
+            <div className="flex-1">Total</div>
+            {/* List outer */}
+            <div>
+              {/* List  */}
+              <ul>
+                {["a", "b", "c", "d"].map((item, index) => {
+                  return (
+                    <li>
+                      <span className="mr-3 text-base-300">Product {item}</span>
+                      <span>costA</span>
+                    </li>
+                  );
+                })}
+                <li>
+                  <span></span>
+                  <span>
+                    {/* Total of all cost */}
+                    <div className="font-bold">Total cost</div>
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Action footer */}
+          <div className="flex flex-row-reverse">
+            <button className="btn btn-primary">Purchase</button>
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/Checkout/CheckoutItem.jsx
+++ client/src/components/Checkout/CheckoutItem.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BsTrash } from "react-icons/bs";
 
-export default function CheckoutItem() {
+export default function CheckoutItem({ itemId }) {
+  const [image, setImage] = useState(null);
+
+  const handleRemoveItem = () => {};
+
   return (
-    <div className='checkOutItem-wrapper '>
-      <div></div>
+    <div className="checkOutItem-wrapper m-h-[180px]">
+      <div className="checkOutItem flex flex-row gap-4">
+        {/* Left */}
+        <div className="w-1/3 ">
+          <div
+            className={`productCard-thumbnail-wrapper w-full bg-cover bg-center h-[140px] sm:h-[180px]`}
+            style={{
+              backgroundImage: `url('${
+                image ? image : `${process.env.PRODUCTION_BASE_URL}/default.png`
+              }')`,
+            }}
+          ></div>
+        </div>
+
+        {/* Right */}
+        <div className="flex-1">
+          {/* Info group */}
+          <div className="text-xl font-bold">Item name</div>
+          <div className="text-xl text-primary font-bold">$ Item price</div>
+        </div>
+
+        {/* Info action */}
+        <div>
+          {/* remove the item */}
+          <div>
+            <button
+              className="btn btn-ghost rounded-full"
+              onClick={handleRemoveItem}
+            >
+              <BsTrash />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
