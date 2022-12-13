31261714c72c1552d00869cb9c7a6bc6dd229be3
Player Nguyên <daudaua3782@gmail.com>
2022-12-13 19:39:45 +0700

    fix: remove .history

--- .history/client/src/components/Sell/Sell_20221204200138.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Sell() {
-  return <div>Something went wrong</div>;
-}
--- .history/client/src/components/Sell/Sell_20221205133026.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-export default function Sell() {
-  return(
-    <div> Hello </div>
-  );
-}
