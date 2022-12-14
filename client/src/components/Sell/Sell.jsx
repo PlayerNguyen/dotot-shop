92006dbe026100cefbaab1779dfa8ebafe690447
Player Nguyên <daudaua3782@gmail.com>
2022-12-14 21:02:26 +0700

    update(fe): styling an ui for sell components

--- client/src/components/Sell/Sell.jsx
+++ client/src/components/Sell/Sell.jsx
@@ -88,19 +88,137 @@
 //   }
 // }
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { FaMoneyBillAlt } from "react-icons/fa";
+import { ResponseInterceptor } from "../../helpers/ResponseInterceptor";
+import CategoryRequest from "../../requests/CategoryRequest";
 
 export default function Sell() {
   const [selectedCategory, setSelectedCategory] = useState(false);
+
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    CategoryRequest.getAllCategory().then((response) => {
+      const { data } = ResponseInterceptor.filterSuccess(response);
+      setCategoryList(data);
+    });
+  }, []);
+
   return (
     <div className="sell-wrapper bg-white p-8 w-full sm:w-4/5 md:w-2/3 lg:w-2/5 mx-auto">
       <div className="sell-container text-base">
+        <div className="mb-12">
+          <b>Dear our valued customer,</b>
+          <p>We are </p>
+        </div>
+        {/* Form control */}
         <div className="form-control">
-          <label>
-            <select className="select select-primary select-sm w-full">
-              <option>A</option>
-            </select>
+          <label className="label">
+            <span className="label-text">Which one you want to sell?</span>
           </label>
+
+          <select className="select select-primary select-sm w-full">
+            {categoryList &&
+              categoryList.map((category) => {
+                return (
+                  <option value={category.Id} key={category.Id}>
+                    {category.Name}
+                  </option>
+                );
+              })}
+          </select>
+        </div>
+
+        {/* Name */}
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name of the product</span>
+          </label>
+          <input
+            className="input input-sm input-primary w-full"
+            name="product-name"
+          />
+        </div>
+
+        {/* Lores */}
+        <div>
+          <label className="label">
+            <span className="label-text">Describe about your furniture</span>
+          </label>
+          {/* <input
+            className="input input-sm input-primary w-full"
+            name="product-description"
+          /> */}
+          <textarea className="textarea textarea-sm textarea-primary w-full"></textarea>
+        </div>
+
+        {/* Condition */}
+        <div>
+          <label className="label">
+            <span className="label-text">Condition of the product</span>
+          </label>
+          {/* <input
+            className="input input-sm input-primary w-full"
+            name="product-description"
+          /> */}
+          <input
+            type="range"
+            min="90"
+            max="100"
+            // value="100"
+            className="range range-primary"
+            // step="1"
+          />
+          <label className="label">
+            <span className="label-text text-base-300">
+              about condition of product
+            </span>
+          </label>
+        </div>
+
+        {/* Price */}
+        <div>
+          <label className="label">
+            <span className="label-text">Cost of product</span>
+          </label>
+
+          <input
+            type="number"
+            min="90"
+            max="100"
+            // value="100"
+            className="input input-sm input-primary w-full"
+            // step="1"
+          />
+          <label className="label">
+            <span className="label-text text-base-300">
+              about condition of product
+            </span>
+          </label>
+        </div>
+
+        {/* Sales */}
+        <div className="form-control ">
+          <label className="label">
+            <span className="label-text">Sales</span>
+          </label>
+          <div className="flex flex-row items-center gap-4">
+            <input type="checkbox" className="toggle toggle-primary" />
+            <div className="flex flex-row gap-4 items-center">
+              <span>New cost after sell</span>
+              <label className="input-group">
+                <span>
+                  <FaMoneyBillAlt />
+                </span>
+                <input
+                  type="number"
+                  className="input input-sm input-primary "
+                />
+                <span>USD</span>
+              </label>
+            </div>
+          </div>
         </div>
       </div>
     </div>
