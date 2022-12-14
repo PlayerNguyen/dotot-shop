41528097f230737dc5d62eda19562fdb223e1c37
Player Nguyên <daudaua3782@gmail.com>
2022-12-14 15:22:13 +0700

    add(front-end): non-selected admin layout

--- client/src/components/Admin/AdminLayout.jsx
+++ client/src/components/Admin/AdminLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { AiFillSetting, AiFillTags } from "react-icons/ai";
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 
 export default function AdminLayout() {
   const { pathname } = useLocation();
@@ -13,9 +13,7 @@ export default function AdminLayout() {
     { name: `Categories`, url: `/admin/categories`, icon: <AiFillTags /> },
   ]);
 
-  useEffect(() => {
-    console.log(pathname);
-  }, [pathname]);
+  const navigate = useNavigate();
 
   return (
     <div className="adminLayout-wrapper">
@@ -38,9 +36,27 @@ export default function AdminLayout() {
             })}
         </div>
         <div className="divider"></div>
-        <div className="px-4 py-3">
-          <Outlet />
-        </div>
+
+        {pathname === "/admin" ? (
+          <div className="flex flex-row flex-wrap gap-6 justify-center mb-4">
+            {adminMenuItems.map((item, idx) => {
+              return (
+                <div
+                  className="w-[89%] sm:w-[47%]  bg-zinc-100 p-3 flex flex-row sm:flex-col justify-center items-center rounded-xl gap-4 hover:bg-zinc-200 cursor-pointer"
+                  key={idx}
+                  onClick={() => navigate(item.url)}
+                >
+                  <div className="text-6xl">{item.icon}</div>
+                  <div className="sm:text-2xl font-bold">{item.name}</div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="px-4 py-3">
+            <Outlet />
+          </div>
+        )}
       </div>
     </div>
   );
