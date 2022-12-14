bdd59709686b7ed1c91a4fde0ef81dac9b7ca8e1
Player Nguyên <daudaua3782@gmail.com>
2022-12-14 20:19:38 +0700

    add(front-end): add category multiple select ui

--- client/src/components/Admin/AdminCategory.jsx
+++ client/src/components/Admin/AdminCategory.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { CgCheckO, CgTrash } from "react-icons/cg";
+import { CgAdd, CgAddR, CgCheckO, CgTrash } from "react-icons/cg";
 import { toast } from "react-toastify";
 import { ResponseInterceptor } from "../../helpers/ResponseInterceptor";
 import useSetState from "../../hooks/useSetState";
@@ -40,9 +40,79 @@ function CategoryItem({ category, isSelected, onSelect, onDelete }) {
   );
 }
 
+function AddCategorySection({ visible, onAdd, onCancel, isAdding }) {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    visible && (
+      <div className="addCategorySection-wrapper">
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-row gap-1">
+            <div>
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type={`text`}
+                className={`input input-sm input-primary w-full`}
+                value={name}
+                name="category-name"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+
+            <div className="flex-1">
+              <label className="label">
+                <span className="label-text">Description</span>
+              </label>
+              <input
+                type={`text`}
+                className={`input input-sm input-primary w-full`}
+                value={description}
+                name="category-description"
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          {/* Footer */}
+          <div className="flex flex-row-reverse gap-3">
+            <button className="btn btn-accent btn-sm" onClick={onCancel}>
+              Cancel
+            </button>
+            <button
+              className={`btn btn-primary btn-sm ${
+                isAdding && isAdding && `loading`
+              }`}
+              disabled={name === "" || isAdding}
+              onClick={() => {
+                onAdd(name, description);
+                // Anti-spam by clear text
+                setName("");
+                setDescription("");
+              }}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  );
+}
+
 export default function AdminCategory() {
   const [categories, setCategories] = useState([]);
   const selectedCategories = useSetState();
+  const [addCategorySectionVisible, setAddCategorySectionVisible] =
+    useState(false);
+
+  const [isAdding, setIsAdding] = useState(false);
+
   const fetchCategory = () => {
     CategoryRequest.getAllCategory()
       .then((response) => {
@@ -79,17 +149,81 @@ export default function AdminCategory() {
       });
   };
 
+  const handleAddSectionToggle = () => {
+    setAddCategorySectionVisible((prev) => !prev);
+  };
+
+  const handleAddNewCategory = (name, description) => {
+    setIsAdding(true);
+    CategoryRequest.createCategory(name, description)
+      .then((response) => {
+        const { data } = ResponseInterceptor.filterSuccess(response);
+        // const {Id, Name, Description, Slug} = data;
+        setCategories([{ ...data }, ...categories]);
+        setAddCategorySectionVisible(false);
+      })
+      .catch((response) => {
+        const { message } = ResponseInterceptor.filterError(response);
+        toast.error(message);
+      })
+      .finally(() => setIsAdding(false));
+  };
+
   return (
     <div className="adminCategory-wrapper">
       <div className="mx-3 mb-3">
-        <div className="font-bold text-3xl">Manage categories</div>
+        <div className="flex flex-row items-center">
+          <div className="font-bold text-3xl flex-1">Manage categories</div>
+          <div>
+            <button
+              className="btn btn-ghost text-primary text-2xl btn-circle"
+              onClick={handleAddSectionToggle}
+            >
+              <CgAdd />
+            </button>
+          </div>
+        </div>
 
+        {/* Add section */}
+        <AddCategorySection
+          visible={addCategorySectionVisible}
+          onCancel={() => {
+            setAddCategorySectionVisible(false);
+          }}
+          isAdding={isAdding}
+          onAdd={handleAddNewCategory}
+        />
+
+        <div className="divider"></div>
+
+        {selectedCategories.state.size > 0 && (
+          <div>
+            <div className="flex flex-row items-center">
+              <div className="flex-1">
+                <b>
+                  {`${selectedCategories.state.size} ${
+                    selectedCategories.state.size > 1
+                      ? `categories`
+                      : `category`
+                  }`}
+                </b>
+              </div>
+              <div className="text-2xl">
+                <button className="btn btn-ghost text-xl btn-circle hover:text-red-600">
+                  <CgTrash />
+                </button>
+              </div>
+            </div>
+            <div className="divider"></div>
+          </div>
+        )}
         {/* List of categories */}
         <div className="my-3">
           {categories &&
             categories.map((category) => {
               return (
                 <CategoryItem
+                  key={category.Id}
                   category={category}
                   isSelected={selectedCategories.state.has(category.Id)}
                   onSelect={handleToggleItem}
